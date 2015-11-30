37db502512936ea86738da5491d7d5113f50f241
Paul Kitchener <p.a.kitchener@gmail.com>
2015-11-30 15:25:48 -0600

    Initial Commit

---
+++ NBABoxScores/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NBABoxScores
+//
+//  Created by Paul Kitchener on 11/30/15.
+//  Copyright © 2015 Paul Kitchener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NBABoxScores/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NBABoxScores
+//
+//  Created by Paul Kitchener on 11/30/15.
+//  Copyright © 2015 Paul Kitchener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
